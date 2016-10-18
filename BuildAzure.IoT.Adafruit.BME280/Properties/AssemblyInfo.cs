f4cb92d61103b3266bd48d5094fc276981a27c10
ta <ta@ecofip.com>
2016-10-18 18:07:35 +0200

    init commit

---
+++ BuildAzure.IoT.Adafruit.BME280/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BuildAzure.IoT.Adafruit.BME280")]
+[assembly: AssemblyDescription("An Adafruit BME280 (Pressure, Temp & Humidity) Sensor library for Windows IoT Core")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Build Azure LLC (http://BuildAzure.com)")]
+[assembly: AssemblyProduct("BuildAzure.IoT.Adafruit.BME280")]
+[assembly: AssemblyCopyright("Copyright © Build Azure LLC 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: ComVisible(false)]
